75e28f2dafa3e141a56a7efecb5dad027a1fe758
Erik Nunes <erikvixx@gmail.com>
2024-01-08 11:58:51 -0300

    feat: include user request

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardContent,
+  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "./components/ui/card";
 import { Input } from "./components/ui/input";
 import { Button } from "./components/ui/button";
+import { Avatar, AvatarImage } from "./components/ui/avatar";
+import { LuGithub, LuMapPin } from "react-icons/lu";
 
-function App() {
-  const [user, setUser] = useState([]);
-  const [userName, setUsername] = useState("");
+interface User {
+  id: number;
+  login: string;
+  avatar_url: string;
+  location: string;
+  bio: string;
+  followers: number;
+  following: number;
+}
 
-  const handleGetUser = (userName: string) => {
-    return fetch(`https://api.github.com/users/${userName}`)
-      .then((response) => response.json())
-      .then(({ users }) => setUser(users))
-      .catch((err) => {
+function App(): JSX.Element {
+  const [users, setUsers] = useState<User[]>([]);
+  const [userName, setUsername] = useState<string>("");
+
+  const handleGetUser = (): void => {
+    fetch(`https://api.github.com/users/${userName}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((userData: User) => {
+        setUsers([userData]);
+      })
+      .catch((err: Error) => {
         console.error(err);
       });
   };
@@ -29,14 +46,52 @@ function App() {
           <CardTitle>Github Search</CardTitle>
           <div className="flex gap-1">
             <Input
-              onChange={(e) => {
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                 setUsername(e.target.value);
               }}
             />
-            <Button onClick={() => handleGetUser(userName)}>Send</Button>
+            <Button onClick={() => handleGetUser()}>Send</Button>
           </div>
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent>
+            <>
+              {users.length > 0 ? (
+                <div>
+                  {users.map((user) => (
+                    <Card key={user.id} className="flex items-center flex-col">
+                      <CardHeader className="flex items-center flex-col">
+                      <Avatar>
+                        <AvatarImage src={user.avatar_url}/>
+                      </Avatar>
+                        <CardTitle>{user.login}</CardTitle>
+                        <CardDescription className="flex items-center gap-1">
+                          <LuMapPin/>
+                          {user.location}</CardDescription>
+                      </CardHeader>
+                      <CardContent>
+                        <CardDescription>{user.bio}</CardDescription>
+                        <div className="flex gap-2">
+                        <CardDescription>Followers: {user.followers}</CardDescription>
+                        <CardDescription>Following: {user.following}</CardDescription>
+                        </div>
+                        <a href={`https://github.com/${userName}?tab=repositories`}>
+                        <Card className="p-4 flex items-center gap-2">
+                          <LuGithub/>
+                          <h1>Repositories</h1>
+                        </Card>
+                        </a>
+                      </CardContent>
+                    </Card>
+                  ))}
+                </div>
+              ) : (
+                <p className="text-gray-500 text-sm font-light">
+                  Enter a GitHub user
+                </p>
+              )}
+            </>
+     
+        </CardContent>
         <CardFooter></CardFooter>
       </Card>
     </div>
