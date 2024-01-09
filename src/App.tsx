31cc4aaadcd8bcb9d22cfca662591c30d2723aad
Erik Nunes <erikvixx@gmail.com>
2024-01-09 08:10:16 -0300

    refactor: location logic

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,7 @@ function App(): JSX.Element {
       })
       .then((userData: User) => {
         setUsers([userData]);
+        console.log(userData)
       })
       .catch((err: Error) => {
         console.error(err);
@@ -54,43 +55,50 @@ function App(): JSX.Element {
           </div>
         </CardHeader>
         <CardContent>
-            <>
-              {users.length > 0 ? (
-                <div>
-                  {users.map((user) => (
-                    <Card key={user.id} className="flex items-center flex-col">
-                      <CardHeader className="flex items-center flex-col">
-                      <Avatar>
-                        <AvatarImage src={user.avatar_url}/>
-                      </Avatar>
-                        <CardTitle>{user.login}</CardTitle>
-                        <CardDescription className="flex items-center gap-1">
-                          <LuMapPin/>
-                          {user.location}</CardDescription>
-                      </CardHeader>
-                      <CardContent>
-                        <CardDescription>{user.bio}</CardDescription>
-                        <div className="flex gap-2">
-                        <CardDescription>Followers: {user.followers}</CardDescription>
-                        <CardDescription>Following: {user.following}</CardDescription>
-                        </div>
-                        <a href={`https://github.com/${userName}?tab=repositories`}>
-                        <Card className="p-4 flex items-center gap-2">
-                          <LuGithub/>
-                          <h1>Repositories</h1>
-                        </Card>
-                        </a>
-                      </CardContent>
-                    </Card>
-                  ))}
-                </div>
-              ) : (
-                <p className="text-gray-500 text-sm font-light">
-                  Enter a GitHub user
-                </p>
-              )}
-            </>
-     
+          {users.length > 0 ? (
+            <div>
+              {users.map((user) => (
+                <Card key={user.id} className="flex items-center flex-col">
+                  <CardHeader className="flex items-center flex-col">
+                    <Avatar>
+                      <AvatarImage src={user.avatar_url} />
+                    </Avatar>
+                    <CardTitle>{user.login}</CardTitle>
+                    {
+                      user.location === null ?  (null) : (
+
+                    <CardDescription className="flex items-center gap-1">
+                      <LuMapPin />  
+                      {user.location}
+                    </CardDescription>
+                      )
+                    }
+                  </CardHeader>
+                  <CardContent>
+                    <CardDescription>{user.bio}</CardDescription>
+                    <div className="flex gap-2">
+                      <CardDescription>
+                        Followers: {user.followers}
+                      </CardDescription>
+                      <CardDescription>
+                        Following: {user.following}
+                      </CardDescription>
+                    </div>
+                    <a href={`https://github.com/${userName}?tab=repositories`}>
+                      <Card className="p-4 flex items-center gap-2">
+                        <LuGithub />
+                        <h1>Repositories</h1>
+                      </Card>
+                    </a>
+                  </CardContent>
+                </Card>
+              ))}
+            </div>
+          ) : (
+            <p className="text-gray-500 text-sm font-light">
+              Enter a GitHub user
+            </p>
+          )}
         </CardContent>
         <CardFooter></CardFooter>
       </Card>
